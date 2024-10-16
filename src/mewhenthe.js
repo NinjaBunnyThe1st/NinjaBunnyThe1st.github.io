9f1089cb310ef94f4fd5fcda33202d09d6b65d4f
NinjaBunnyThe1st <frank.401323@nv.ccsd.net>
2024-10-16 10:26:38 -0700

    I feel so sigma!

--- src/mewhenthe.js
+++ src/mewhenthe.js
@@ -1,9 +1,28 @@
-function ohio(evt,name){var i,tabcontent,tablinks;tabcontent=document.getElementsByClassName("tabcontent")
+
+//The "Ohio" function is for the tab functionallty 
+//the b1 size is 900px meant to be open for side images.
+
+function ohio(evt, name) {
+  var i, tabcontent, tablinks; tabcontent = document.getElementsByClassName("tabcontent")
     for(i=0;i<tabcontent.length;i++){tabcontent[i].style.display="none"}
     tablinks=document.getElementsByClassName("tablinks");for(i=0;i<tablinks.length;i++){
     tablinks[i].className=tablinks[i].className.replace(" active","")}
    document.getElementById(name).style.display="block";evt.currentTarget.className+=" active"}
-     ohio("tablinks","Home");
-   function grands(){
-
-   }
+    ohio("tablinks", "Home");
+     
+function grands() {
+  var d = document.getElementById;
+  var x = d("ordered");
+  var y = d("yepe");
+  if (y.value != "") {
+    x.innerHTML = "<h2>Cool</h2>";
+    sleep(2000);
+    x.innerHTML = "";
+  }
+  else {
+    x.innerHTML = "<h2>Goofy</h2>";
+    sleep(2000);
+    x.innerHTML = "";
+  }
+}
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
