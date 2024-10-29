9f6fa2723ca2fa3894f835d5b0a92bed6c494b49
NinjaBunnyThe1st <frank.401323@nv.ccsd.net>
2024-10-29 10:40:45 -0700

    :_c

--- src/mewhenthe.js
+++ src/mewhenthe.js
@@ -2,16 +2,18 @@
 //The "Ohio" function is for the tab functionallty 
 //the b1 size is 900px meant to be open for side images.
 
+style.backgroundColor = 'green';
+var d = document.getElementById;
+
 function ohio(evt, name) {
   var i, tabcontent, tablinks; tabcontent = document.getElementsByClassName("tabcontent")
     for(i=0;i<tabcontent.length;i++){tabcontent[i].style.display="none"}
     tablinks=document.getElementsByClassName("tablinks");for(i=0;i<tablinks.length;i++){
-    tablinks[i].className=tablinks[i].className.replace(" active","")}
+      tablinks[i].className=tablinks[i].className.replace(" active","")}
    document.getElementById(name).style.display="block";evt.currentTarget.className+=" active"}
     ohio("tablinks", "Home");
      
 function grands() {
-  var d = document.getElementById;
   var x = d("ordered");
   var y = d("yepe");
   if (y.innerHTML != "") {
